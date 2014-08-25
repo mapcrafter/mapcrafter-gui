301c423e349a8b24c87277f829aeef4eadaa1294
Moritz Hilscher <moritz.hilscher@gmail.com>
2014-08-25 21:50:51 +0200

    Improved syntax highlighting a bit, but concrete colors still not sure..

--- inisyntaxhighlighter.cpp
+++ inisyntaxhighlighter.cpp
@@ -5,10 +5,11 @@ INISyntaxHighlighter::INISyntaxHighlighter(QTextDocument *parent) :
 }
 
 void INISyntaxHighlighter::highlightBlock(const QString& text) {
-    QTextCharFormat commentFormat, sectionFormat, optionFormat;
-    commentFormat.setForeground(Qt::blue);
+    QTextCharFormat commentFormat, sectionFormat, keyFormat, valueSpecialFormat;
+    commentFormat.setForeground(Qt::darkGreen);
     sectionFormat.setForeground(Qt::red);
-    optionFormat.setForeground(Qt::darkGreen);
+    keyFormat.setForeground(Qt::darkCyan);
+    valueSpecialFormat.setForeground(Qt::blue);
 
     QStringList lines = text.split("\n");
     int startIndex = 0, endIndex = 0;
@@ -19,8 +20,20 @@ void INISyntaxHighlighter::highlightBlock(const QString& text) {
             setFormat(startIndex, endIndex, commentFormat);
         else if (line.trimmed().startsWith("["))
             setFormat(startIndex, endIndex, sectionFormat);
-        else
-            setFormat(startIndex, endIndex, optionFormat);
+        else {
+            int mid = line.indexOf('=');
+            if (mid == -1)
+                mid = line.size();
+            setFormat(startIndex, startIndex + mid + 1, keyFormat);
+
+            QString key = line.mid(0, mid);
+            QString value = line.mid(mid+1);
+
+            bool isNum;
+            value.toDouble(&isNum);
+            if (isNum || value == "true" || value == "false")
+                setFormat(startIndex + mid + 1, endIndex, valueSpecialFormat);
+        }
         startIndex = endIndex;
     }
 }
